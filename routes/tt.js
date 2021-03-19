26825f842aca41cffdc7005a218ff71772c0e150
xiaohai-huang <jeromehuang18@gmail.com>
2021-03-20 01:09:18 +1000

    Minnor fixs.

--- routes/tt.js
+++ routes/tt.js
@@ -106,8 +106,7 @@ router.get("/videos/:newsId", async function (req, res) {
     const url = `https://m.toutiaoimg.cn/i${newsId}/?w2atif=1&channel=video`;
     console.log(url);
     const browser = await puppeteer.launch({
-      headless: false,
-      args: ["--no-sandbox"],
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
     const page = await browser.newPage();
     await page.emulate(iPhone);
