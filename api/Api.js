5d3f4a728687c9fa69330c2c43104a367f6b08e5
xiaohai-huang <jeromehuang18@gmail.com>
2021-03-30 15:32:54 +1000

    Use cUrl (bash) to fetch initial news.

--- api/Api.js
+++ api/Api.js
@@ -1,24 +1,33 @@
 const axios = require("axios");
 axios.default.withCredential = true;
+const { promisify } = require("util");
+const exec = promisify(require("child_process").exec);
 
 let Api = {};
 
-Api.getInitialNews = (category) => {
-  return axios(
-    `https://www.toutiao.com/api/pc/feed/?min_behot_time=0&category=${category}&utm_source=toutiao&widen=1&tadrequire=true&_signature=_02B4Z6wo00f01ZKWs8AAAIDCSZNPhGgVOamSs7dAAASfSzg0K.WOLW57Bp2-xYZXzK-1gB2MJIucwnNzdJP7rnJ1xZjMUdFm2rPsi1Y5NtbZAXuZyIbS.UkyMPubMkPc7l.8dL5d7VWYYPeSef`,
-    {
-      credentials: "include",
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0",
-        Accept: "application/json, text/plain, */*",
-        "Accept-Language": "en-US,en;q=0.5",
-      },
-      referrer: "https://www.toutiao.com/ch/news_tech/",
-      method: "GET",
-      mode: "cors",
-    }
-  ).then((res) => res.data);
+// Api.getInitialNews = (category) => {
+//   return axios(
+//     `https://www.toutiao.com/api/pc/feed/?min_behot_time=0&category=${category}&utm_source=toutiao&widen=1&tadrequire=true&_signature=_02B4Z6wo00f01ZKWs8AAAIDCSZNPhGgVOamSs7dAAASfSzg0K.WOLW57Bp2-xYZXzK-1gB2MJIucwnNzdJP7rnJ1xZjMUdFm2rPsi1Y5NtbZAXuZyIbS.UkyMPubMkPc7l.8dL5d7VWYYPeSef`,
+//     {
+//       credentials: "include",
+//       headers: {
+//         "User-Agent":
+//           "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0",
+//         Accept: "application/json, text/plain, */*",
+//         "Accept-Language": "en-US,en;q=0.5",
+//       },
+//       referrer: "https://www.toutiao.com/ch/news_tech/",
+//       method: "GET",
+//       mode: "cors",
+//     }
+//   ).then((res) => res.data);
+// };
+Api.getInitialNews = async function (category) {
+  const news = await exec(
+    `curl 'https://www.toutiao.com/api/pc/feed/?min_behot_time=0&category=${category}&utm_source=toutiao&widen=1&tadrequire=true&_signature=_02B4Z6wo00f01jBOEVgAAIDCMun48T6coyYwaxXAAOyAwvu5JQJMD7lMZmlQ1YLPucgM8Y7wXByCAakFSpO6kvzQRMNghk-viGDprLeDVUaa9zncHfoz9AIoV2AiKFSYp2bhHqzKImaKGWO866' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0' -H 'Accept: application/json, text/plain, */*' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Connection: keep-alive' -H 'Referer: https://www.toutiao.com/' -H 'Cookie: tt_webid=6932428093810492941; MONITOR_WEB_ID=0ac2db7b-8fc4-4e00-9856-f6df8bdbd79e; csrftoken=61e2446e34a7c4322be05ba8cb8e5a92; tt_webid=6932428093810492941; ttcid=dd7046f16bd246ef99679f2ede9a899338; passport_csrf_token=e430036a3bcbcc9ef38d92cb85ecd745; passport_csrf_token_default=e430036a3bcbcc9ef38d92cb85ecd745; __ac_signature=_02B4Z6wo00f01N8cUUAAAIDDBBmtB3NAAkjfOVXAAFebiPSMUlNiiLcF9.cr-LBYf02ILiMmZIjFeD2jAL3Da4shTBkrpz8YVylUKXl6gdhKaUo0oncScZEMHuVg84cdxwn75veJ.7TJKzZBc0; tt_scid=cl9Ppo91wU9Hd6562Jf5mwV.kdUjC1JdfqnVCIMIGJHYibJTznSAO7GO-m4zCx7k868e' -H 'TE: Trailers'`,
+    { encoding: "UTF-8" }
+  );
+  return JSON.parse(news.stdout);
 };
 
 Api.getNews = (category, max_behot_time) => {
